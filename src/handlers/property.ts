3da8b982a3f693b4f6c9134213ba3eba5ebfc610
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-21 03:59:33 +0300

    organize

---
+++ src/handlers/property.ts
@@ -0,0 +1,80 @@
+import { PrismaClient } from "@prisma/client";
+import prisma from "../db";
+
+const getAllProperties = async (req, res) => {
+  const { page, limit, ...filters } = req.query;
+
+  const pageNumber = parseInt(page as string) || 1;
+  const limitNumber = parseInt(limit as string) || 2;
+
+  const startIndex = (pageNumber - 1) * limitNumber;
+  const endIndex = pageNumber * limitNumber;
+
+  const properties = await prisma.property.findMany();
+
+  const filteredProperties = properties.filter((property) => {
+    return Object.keys(filters).every((filter) => {
+      return property[filter] === filters[filter];
+    });
+  });
+
+  const data = filteredProperties.slice(startIndex, endIndex);
+
+  const result = {
+    all_items: filteredProperties.length,
+    page: pageNumber,
+    max_page: Math.ceil(filteredProperties.length / limitNumber),
+    limit: limitNumber,
+    data,
+  };
+  return res.json(result);
+};
+
+const getPropertyById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const property = await prisma.property.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return res.json(property);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Property not found or an error occurred" });
+  }
+};
+
+const createProperty = async (req, res) => {
+  const { title, vibe, amenities, photos, days, contact, price } = req.body;
+
+  try {
+    const property = await prisma.property.create({
+      data: {
+        title,
+        vibe,
+        amenities,
+        photos,
+        days: {
+          create: days,
+        },
+        contact: {
+          create: contact, // Create related Contact record
+        },
+        price: {
+          create: price, // Create related Price record
+        },
+      },
+    });
+
+    return res.json(property);
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export { getAllProperties, getPropertyById, createProperty };
--- src/router.ts
+++ src/router.ts
@@ -1,57 +1,14 @@
 import { Router } from "express";
-import prisma from "./db";
+import {
+  getAllProperties,
+  getPropertyById,
+  createProperty,
+} from "./handlers/property";
 
 const router = Router();
 
-router.get("/property", async (req, res) => {
-  const { page, limit, ...filters } = req.query;
-
-  const pageNumber = parseInt(page as string) || 1;
-  const limitNumber = parseInt(limit as string) || 2;
-
-  const startIndex = (pageNumber - 1) * limitNumber;
-  const endIndex = pageNumber * limitNumber;
-
-  const properties = await prisma.property.findMany();
-
-  const filteredProperties = properties.filter((property) => {
-    return Object.keys(filters).every((filter) => {
-      return property[filter] === filters[filter];
-    });
-  });
-
-  const data = filteredProperties.slice(startIndex, endIndex);
-
-  const result = {
-    all_items: filteredProperties.length,
-    page: pageNumber,
-    max_page: Math.ceil(filteredProperties.length / limitNumber),
-    limit: limitNumber,
-    data,
-  };
-  return res.json(result);
-});
-
-router.get("/property/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const property = await prisma.property.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    return res.json(property);
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ error: "Property not found or an error occurred" });
-  }
-});
-
-router.put("/property/:id", (req, res) => {});
-router.post("/property", (req, res) => {});
-router.delete("/property/:id", (req, res) => {});
+router.get("/property", getAllProperties);
+router.get("/property/:id", getPropertyById);
+router.post("/property", createProperty);
 
 export default router;
