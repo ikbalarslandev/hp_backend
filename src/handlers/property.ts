c9cc732fc9eecae56fcadedcee149df860f10d88
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-01 01:49:49 +0300

    sort params bug fix

--- src/handlers/property.ts
+++ src/handlers/property.ts
@@ -32,11 +32,11 @@ const getAllProperties = async (req, res) => {
     const priceA = a.price.adult;
     const priceB = b.price.adult;
 
-    if (sort === "to-cheap") {
+    if (sort === "to-expensive") {
       return priceA - priceB;
     }
 
-    if (sort === "to-expensive") {
+    if (sort === "to-cheap") {
       return priceB - priceA;
     }
 
